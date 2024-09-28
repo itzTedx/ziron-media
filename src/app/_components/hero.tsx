d2c125264e2bd3af1aa73c08045aa573949cf52e
melwin <114172287+itzTedx@users.noreply.github.com>
2024-09-28 12:36:04 +0400

    added: New Font - Mona Sans Bold

--- src/app/_components/hero.tsx
+++ src/app/_components/hero.tsx
@@ -9,11 +9,11 @@ import { cn } from "@/lib/utils";
 export default function Hero() {
   return (
     <section className="container text-center">
-      <h1 className="mt-20 text-3xl font-bold tracking-tight text-primary md:text-6xl md:leading-[4rem]">
+      <h1 className="font-monaSans mt-32 text-3xl font-bold tracking-tight text-primary md:text-7xl md:leading-[4rem]">
         We’re great at Digital <br />
         Marketing Solutions
       </h1>
-      <p className="mt-3 text-balance">
+      <p className="mt-6 text-balance text-lg">
         We specialize in brand identities, scalable websites, and expert social
         media management
       </p>
@@ -21,15 +21,15 @@ export default function Hero() {
       <Link
         href="/contact"
         className={cn(
-          buttonVariants({ variant: "outline" }),
-          "mt-12 font-bold uppercase text-primary"
+          buttonVariants({ variant: "outline", size: "lg" }),
+          "mt-12 h-12 font-bold uppercase text-primary"
         )}
       >
         Let&apos;s get started <IconArrowRight className="ml-2 size-5" />
       </Link>
 
       <HeroVideoDialog
-        className="mx-auto mt-16 max-w-5xl"
+        className="mx-auto mt-24 max-w-5xl"
         animationStyle="from-center"
         videoSrc="https://www.youtube.com/embed/LDU_Txk06tM?si=I2jLBKfxTPB0i127"
         thumbnailSrc="https://startup-template-sage.vercel.app/hero-light.png"
