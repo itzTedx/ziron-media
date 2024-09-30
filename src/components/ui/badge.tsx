a36c2838a0a165be323a28873f2d91f5bcf77e13
melwin <114172287+itzTedx@users.noreply.github.com>
2024-09-30 10:27:05 +0400

    hover animation fixes

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -5,12 +5,12 @@ import { type VariantProps, cva } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
 const badgeVariants = cva(
-  "inline-flex items-center rounded-md border px-6 py-1.5 text-sm transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 rounded-full uppercase w-fit",
+  "inline-flex items-center rounded-md border px-6 py-1.5 text-sm transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 rounded-full uppercase w-fit duration-500",
   {
     variants: {
       variant: {
-        default: "border-primary text-primary hover:bg-primary/80",
-        secondary: "border-secondary text-secondary hover:bg-secondary/80",
+        default: "border-primary text-primary hover:bg-primary/5",
+        secondary: "border-secondary text-secondary hover:bg-secondary/5",
         destructive:
           "border-transparent bg-destructive text-destructive-foreground shadow hover:bg-destructive/80",
         outline: "text-foreground",
@@ -29,7 +29,7 @@ export interface BadgeProps
 function Badge({ className, variant, ...props }: BadgeProps) {
   return (
     <div
-      className={cn(badgeVariants({ variant }), className, "p-")}
+      className={cn(badgeVariants({ variant }), className, "cursor-default")}
       {...props}
     />
   );
