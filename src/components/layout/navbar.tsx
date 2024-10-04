f7f803f1e920198b149eff0bbf5f086c777d8b70
melwin <114172287+itzTedx@users.noreply.github.com>
2024-10-04 16:25:33 +0400

    navbar bg color while not scrolled

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -22,7 +22,7 @@ export default function Navbar() {
             "hidden w-full items-center justify-between rounded-full border border-background bg-[#FAF9FB] px-9 py-3 shadow-glow transition-all duration-500 ease-in-out max-lg:text-sm md:flex",
             isScrolled
               ? "bg-[#FAF9FB]/50 backdrop-blur-md"
-              : "bg-transparent backdrop-blur-0"
+              : "bg-[#FAF9FB] backdrop-blur-0"
           )}
           style={{
             transitionProperty: "background-color, backdrop-filter, margin-top",
