64866a56956523de832098416f31a4b23e12602e
melwin <114172287+itzTedx@users.noreply.github.com>
2024-10-16 14:00:07 +0400

    SEO

--- src/utils/site-config.ts
+++ src/utils/site-config.ts
@@ -4,10 +4,10 @@ export const siteConfig: SiteConfig = {
   name: "Ziron Media: Digital Marketing Agency for Online Growth ",
   shortName: "Ziron Media",
   description:
-    "Ziron Media specialize in brand identities, scalable websites, and expert social media management based in Dubai",
+    "We specialized in brand identities, scalable websites, and expert social media management based in Dubai",
 
-  url: "https://ziron.vercel.app",
-  ogImage: "/og-image.jpg",
+  url: "https://www.zironmedia.com",
+  ogImage: "/og.jpg",
   links: {
     twitter: "https://x.com/itzTedx_",
   },
