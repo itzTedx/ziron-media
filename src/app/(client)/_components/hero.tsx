c7fd9967123989bdbf0ccaf21e827bc28e85ecec
melwin <114172287+itzTedx@users.noreply.github.com>
2024-10-24 10:53:54 +0400

    Fix: UI

--- src/app/(client)/_components/hero.tsx
+++ src/app/(client)/_components/hero.tsx
@@ -22,10 +22,10 @@ export default async function Hero() {
       <div className="absolute inset-0 -z-10 h-full w-full bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:20px_24px]">
         <div className="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-fuchsia-400 opacity-20 blur-[100px]"></div>
       </div>
-      <h1 className="pt-12 text-center font-monaSans text-[2rem] font-bold tracking-tight text-gray-900 md:pt-32 md:text-7xl md:leading-[5rem]">
+      <h1 className="pt-24 text-center font-monaSans text-[2.5rem] font-bold tracking-tight text-gray-900 md:pt-32 md:text-7xl md:leading-[5rem]">
         We’re great at{" "}
         <span className="text-primary">
-          Digital <br />
+          Digital <br className="hidden md:block" />
           Marketing
         </span>{" "}
         Solutions
