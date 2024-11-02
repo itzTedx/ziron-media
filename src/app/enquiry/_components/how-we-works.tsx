0b865cd56c6a014a733b99b4c2682d4a89ff50c3
melwin <114172287+itzTedx@users.noreply.github.com>
2024-11-02 11:22:01 +0400

    Enquiry Page UI done

--- src/app/enquiry/_components/how-we-works.tsx
+++ src/app/enquiry/_components/how-we-works.tsx
@@ -1,3 +1,63 @@
+import Image from "next/image";
+
+import { Badge } from "@/components/ui/badge";
+
 export const HowWeWorks = () => {
-  return <div>HowWeWorks</div>;
+  return (
+    <section className="container pb-40">
+      <Badge>✦ This is how we works</Badge>
+      <h3 className="py-3 text-5xl">Your Dedicated Digital Partner</h3>
+      <p>Here&apos;s how we tailor our approach to ensure your success.</p>
+      <div className="grid grid-cols-3 gap-6 pt-9">
+        <div className="flex flex-col justify-between rounded-xl bg-gray-950 text-gray-50">
+          <div className="p-12">
+            <h4 className="text-2xl font-bold">Data-Driven Approach</h4>
+            <p className="text-balance pt-3 text-xl">
+              We use in-depth analytics to inform our strategies and maximize
+              your ROI.
+            </p>
+          </div>
+          <Image
+            src="/images/data-illustration.svg"
+            height={206}
+            width={350}
+            alt="Small Business growth is in digital marketing agencies hands"
+            className="mx-auto py-12"
+          />
+        </div>
+        <div className="flex flex-col justify-between rounded-xl bg-violet-300">
+          <div className="p-12">
+            <h4 className="text-2xl font-bold">Tailored Solutions</h4>
+            <p className="text-balance pt-3 text-xl">
+              Every business is unique, and we customize our services to fit
+              your specific needs.
+            </p>
+          </div>
+          <Image
+            src="/images/puzzle-hands.svg"
+            height={206}
+            width={300}
+            alt="Problem Solving in ease"
+            className="w-full pt-6"
+          />
+        </div>
+        <div className="flex flex-col justify-between rounded-xl bg-gray-50">
+          <div className="p-12">
+            <h4 className="text-2xl font-bold">Ongoing Support</h4>
+            <p className="text-balance pt-3 text-xl">
+              Our commitment to your success extends beyond initial campaigns,
+              providing continuous insights and adjustments.
+            </p>
+          </div>
+          <Image
+            src="/images/group.svg"
+            height={206}
+            width={300}
+            alt="Ready to serve customer services"
+            className="mx-auto py-6"
+          />
+        </div>
+      </div>
+    </section>
+  );
 };
--- src/app/enquiry/_components/services.tsx
+++ src/app/enquiry/_components/services.tsx
@@ -1,3 +1,63 @@
-export const Services = () => {
-  return <div>Services</div>;
+import Link from "next/link";
+
+import { IconArrowUpRight } from "@tabler/icons-react";
+
+import { getServices } from "@/server/actions/get-service-by-mdx";
+
+export const Services = async () => {
+  const services = await getServices();
+  return (
+    <section className="container relative grid grid-cols-2 gap-4 py-28 md:grid-cols-4">
+      {services.splice(0, 7).map((service) => (
+        <Link
+          href={service.slug}
+          key={service.id}
+          className="z-20 flex flex-col justify-between rounded-md border bg-gray-50 p-6"
+        >
+          <h5 className="text-lg font-semibold">
+            {service.title
+              ?.split(" ")
+              .map((word, index) => {
+                return index === 0 ? (
+                  <span key={index}>
+                    {word}
+                    <br />
+                  </span>
+                ) : (
+                  <span key={index} className="text-muted-foreground">
+                    {word}{" "}
+                  </span>
+                );
+              })
+              .flat()}
+          </h5>
+          <div className="flex items-center gap-2 pt-6">
+            <div className="rounded-full border p-1">
+              <IconArrowUpRight className="size-5" />
+            </div>
+            Learn More
+          </div>
+        </Link>
+      ))}
+      <Link
+        href={"/what-we-do"}
+        className="flex flex-col justify-between rounded-md border bg-gray-50 p-6"
+      >
+        <h5 className="text-lg font-semibold">
+          <span className="text-muted-foreground">Explore our</span>
+          <br />
+          Services
+        </h5>
+        <div className="flex items-center gap-2 pt-6">
+          <div className="rounded-full border p-1">
+            <IconArrowUpRight className="size-5" />
+          </div>
+          Learn More
+        </div>
+      </Link>
+      <div className="absolute inset-0 z-10 h-full w-full bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:20px_24px]">
+        <div className="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-purple-400 opacity-20 blur-[100px]"></div>
+      </div>
+    </section>
+  );
 };
