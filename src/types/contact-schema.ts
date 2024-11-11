b7bc5400ef30b8a49c655d52c768f22775e46cf1
melwin <114172287+itzTedx@users.noreply.github.com>
2024-11-11 12:48:50 +0400

    fix: contact form type validation

--- src/types/contact-schema.ts
+++ src/types/contact-schema.ts
@@ -6,8 +6,7 @@ export const contactSchema = z.object({
     .min(2, { message: "Please enter your name" })
     .max(150, { message: "There is no way your name this long" }),
   email: z.string().email(),
-  phone: z.string().optional(),
-  service: z.string({ message: "Please select any of our services" }),
+
   message: z.string().min(2, { message: "Write something.." }).max(256),
 });
 
